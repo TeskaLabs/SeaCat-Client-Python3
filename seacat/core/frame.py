5b3adb8121d990b25c9ebd3c20f3bc3fb0aed026
Ales Teska <ales.teska@teskalabs.com>
2016-12-18 20:17:34 +0000

    Frames are now correctly reset prior use.

--- seacat/core/frame.py
+++ seacat/core/frame.py
@@ -4,10 +4,12 @@ class Frame(object):
 
 	def __init__(self, capacity):
 		self.capacity = capacity
+		self.data = ctypes.create_string_buffer(capacity)
+		self.reset()
+
+	def reset(self):
 		self.position = 0
 		self.limit = self.capacity
-		self.data = ctypes.create_string_buffer(capacity)
-
 
 	def flip(self):
 		self.limit = self.position
